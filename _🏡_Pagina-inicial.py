9ac67c5c69be24c06fef6e174827e73d25efe667
Davidson Santana <davidsonalves1989@gmail.com>
2023-07-17 18:37:53 -0300

    versao-20230717-1837

---
+++ _🏡_Pagina-inicial.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(page_title='Página Inicial',
+                   page_icon='🏡')
+
+st.write('# Sejam-vindos ao FutMat ⚽🎲📊')
+
+st.markdown("""
+            Neste app vocês encontram análises estatísticas sobre os campeonatos brasileiros Série
+            A e B, feitas pelo Professor Davidson Santana👨🏽‍🏫.
+            
+            👈🏽Selecione uma das opções ao lado para navegar entre as análises para o Brasileirão Série A ou B.
+            """)
---
+++ pages/2_🅱️_Brasileirão Série B.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+#página do Brasileirão Série A
+
+st.write('# Em construção.⛏️🚧')
