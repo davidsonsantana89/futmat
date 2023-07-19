f09c72dc28af4eb6b4cbb45229280e11bf5d6c3b
Davidson Santana <davidsonalves1989@gmail.com>
2023-07-19 15:45:35 -0300

    Upload planilhas - 2023.07.19 - 1545

--- pages/1_🅰️_Brasileirão Série A.py
+++ pages/1_🅰️_Brasileirão Série A.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 #página do Brasileirão Série A
 
@@ -8,3 +10,13 @@ st.set_page_config(page_title='🅰️ Brasileirão Série A',
 st.write("# 🅰️ Brasileirão Série A")
 
 st.write('### Em construção.⛏️🚧')
+
+########## INSERÇÃO DE DADOS ##############
+@st.cache
+
+DATA_URL = ('https://github.com/davidsonsantana89/futmat/blob/main/planilhas/serie-a/serie-a-gols.xlsx'
+)
+data = pd.read_excel(DATA_URL)
+st.write(data)
+
+# hist_values = np.histogram()
