b7bb56a0176d7bb2eb084637e26f4349014af860
Davidson Santana <davidsonalves1989@gmail.com>
2023-07-19 16:11:07 -0300

    upload

--- pages/1_🅰️_Brasileirão Série A.py
+++ pages/1_🅰️_Brasileirão Série A.py
@@ -13,11 +13,12 @@ st.write("# 🅰️ Brasileirão Série A")
 
 ########## INSERÇÃO DE DADOS ##############
 # DATA_URL = r"D:\STREAMLIT\futmat\planilhas\serie-a\serie-a-gols.csv"
+DATA_URL = "https://github.com/davidsonsantana89/futmat/blob/main/planilhas/serie-a/serie-a-gols.csv"
 
 def get_data():
-    return pd.read_csv(DATA_URL, sep=',')
+    return pd.read_csv(DATA_URL, sep=';')
 
-DATA_URL = "https://github.com/davidsonsantana89/futmat/blob/main/planilhas/serie-a/serie-a-gols.csv"
+
 df = get_data()
 st.write(df)
 
